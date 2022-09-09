e481814ee7950a33791247f9c58514ed3de7f962
unknown <zx053a@yandex.ru>
2022-09-09 10:11:19 +0300

    first UI

--- WindowsFormsApp1/FrmMain.Designer.cs
+++ WindowsFormsApp1/FrmMain.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class FrmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,18 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmMain";
+            this.Text = "omegaBrowser";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- WindowsFormsApp1/FrmMain.cs
+++ WindowsFormsApp1/FrmMain.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : MaterialForm
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey200, Primary.Grey300, Primary.Grey100, Accent.Teal400, TextShade.BLACK);
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
